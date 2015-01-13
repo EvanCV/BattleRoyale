7e7c50152f7f0225528f78146187a3f75f32ea02
EvanCV <evan.c.vandenberg@gmail.com>
2015-01-13 13:58:44 -0800

    Initial Commit

---
+++ MCMS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCMS
+//
+//  Created by Evan Vandenberg on 1/13/15.
+//  Copyright (c) 2015 Evan Vandenberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MCMS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCMS
+//
+//  Created by Evan Vandenberg on 1/13/15.
+//  Copyright (c) 2015 Evan Vandenberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
